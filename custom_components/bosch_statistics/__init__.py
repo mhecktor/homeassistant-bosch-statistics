2502aed5b82eb41aad524b7e845bdb0550958434
Marco Hecktor <marco.hecktor@fh-dortmund.de>
2026-04-29 09:19:53 +0200

    Removed hass data pop

--- custom_components/bosch_statistics/__init__.py
+++ custom_components/bosch_statistics/__init__.py
@@ -74,7 +74,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
+    # if unload_ok:
+    # hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
